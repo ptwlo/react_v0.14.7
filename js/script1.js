05298046c02e6280d1b78e9698b6e376fb808164
Petter Lopez <petterlopez003@gmail.com>
2020-03-27 23:53:37 -0400

    minor fixes

---
+++ js/script1.js
@@ -0,0 +1,9 @@
+//const React = require('react');
+var HelloWorld = React.createClass({
+  render: function(){
+    return(
+      <h1>Petter Parker</h1>
+    );
+  }
+});
+React.render(<HelloWorld/>, document.getElementById('root'));
