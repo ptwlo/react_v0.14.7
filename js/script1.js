d3d220d26b522f735945feb54f00d948546b14b4
Petter Lopez <petterlopez003@gmail.com>
2020-03-28 21:53:37 -0400

    adding addons & fixes

--- js/script1.js
+++ js/script1.js
@@ -1,9 +1,44 @@
-//const React = require('react');
-var HelloWorld = React.createClass({
+var Header = React.createClass({
   render: function(){
     return(
-      <h1>Petter Parker</h1>
+      <nav>
+        <h1>NavBit</h1>
+        {this.props.children}
+      </nav>
     );
   }
 });
-React.render(<HelloWorld/>, document.getElementById('root'));
+var Clock = React.createClass({
+  render: function(){
+    return (
+      <span>{new Date().toLocaleTimeString()}</span>
+    );
+  }
+});
+var List = React.createClass({
+  render: function(){
+    return (
+      <ul>
+        <li>{this.props.fName}</li>
+        <li>{this.props.lName}</li>
+        <li>{this.props.age}</li>
+      </ul>
+    );
+  }
+});
+var Content = React.createClass({
+  render: function(){
+    return(
+      <div>
+      <Header>
+        <hr/>
+        <h2>{this.props.h2}</h2>
+        <Clock/>
+      </Header>
+      <hr/>
+      <List fName={"Petter"} lName={"Lopez"} age={30} />
+      </div>
+    );
+  }
+});
+React.render(<Content h2="This is H2."/>, document.getElementById('root'));
